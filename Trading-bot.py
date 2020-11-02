d3af95da32e8fae57369d1b59ce863e0de6ad715
Brennan <61703271+BrennanCode1@users.noreply.github.com>
2020-11-02 13:54:26 -0800

    PLaced in functions refreshing evreytime with threading

--- Trading-bot.py
+++ Trading-bot.py
@@ -2,24 +2,46 @@ from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.foreignexchange import ForeignExchange
 from pprint import pprint
 from apikey import api_key
+import threading 
 import pandas as pd
 import json 
 import requests
 import time
 
 
+def currentPriceApiCall():
+    for i in range (5):
+        threading.Timer(20.0, currentPriceApiCall).start()
+        cc = ForeignExchange(key=api_key)
+        data, _ = cc.get_currency_exchange_rate(from_currency='BTC',to_currency='USD')
+        currentPrice=float (data["5. Exchange Rate"])
+        i+=1
+        print ("Current: " , currentPrice)
+    return currentPrice
 
+def fiveMinPriceApiCall():
+    for i2 in range (1):
+        threading.Timer(100.0, fiveMinPriceApiCall).start()
+        cc = ForeignExchange(key=api_key)
+        data, _ = cc.get_currency_exchange_rate(from_currency='BTC',to_currency='USD')
+        fiveMinPrice=float (data["5. Exchange Rate"])
+        print ("Five: " , fiveMinPrice)
+        i2+=1
+    return fiveMinPrice    
+
+i2=0
 i=0
 lastPrice=1
-for i in range (5):
-    cc = ForeignExchange(key=api_key)
-    data, _ = cc.get_currency_exchange_rate(from_currency='BTC',to_currency='USD')
-    currentPrice=float (data["5. Exchange Rate"])
-    compare = (lastPrice / currentPrice) * 100
-    print(compare)
-    lastPrice=currentPrice
-    print(lastPrice)
-    time.sleep (20)
-    i +=1
+
+currentPriceApiCall()
+fiveMinPriceApiCall()
+
+currentPrice=currentPriceApiCall()
+compare = (lastPrice / currentPrice) * 100 
+print("compare",compare)
+lastPrice=currentPrice
+print("lastprice",lastPrice)
+
+
 
 
