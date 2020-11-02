ee9e4444dc84882b9e9efd602b84771a75c951af
Brennan <61703271+BrennanCode1@users.noreply.github.com>
2020-11-02 13:11:07 -0800

    Update Trading-bot.py

--- Trading-bot.py
+++ Trading-bot.py
@@ -6,15 +6,18 @@ import pandas as pd
 import json 
 import requests
 import time
-
-
-
 i=0
 lastPrice=1
-for i in range (5):
+currentBank=500 
+
+def refreshApi():
     cc = ForeignExchange(key=api_key)
     data, _ = cc.get_currency_exchange_rate(from_currency='BTC',to_currency='USD')
     currentPrice=float (data["5. Exchange Rate"])
+
+
+for i in range (5):
+    refreshApi(currentPrice)
     compare = (lastPrice / currentPrice) * 100
     print(compare)
     lastPrice=currentPrice
